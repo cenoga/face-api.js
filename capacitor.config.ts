27eb04ca625ec3ca62704fe1495863a05712f1cd
cenoga38 <cenoga38@gmail.com>
2023-07-13 08:41:59 +0800

    Add existing project files to Git

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ai_face_detection',
+  appName: 'ai_face_detection',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
